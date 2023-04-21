96b96b2edd0dc5ff302c82ebeee75adff11f9350
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-04-21 15:37:04 -0600

    format

--- vercel-deployment-task/test/index.test.ts
+++ vercel-deployment-task/test/index.test.ts
@@ -1,10 +1,10 @@
-import tap from 'tap';
-import { MockTestRunner } from 'azure-pipelines-task-lib/mock-test';
+import tap from "tap";
+import { MockTestRunner } from "azure-pipelines-task-lib/mock-test";
 
-tap.test('ok', t => {
-    t.plan(2);
-    const testRunner = new MockTestRunner(require.resolve('./success'));
-    testRunner.run();
-    t.equal(testRunner.succeeded, true);
-    t.ok(testRunner.stdout.includes('Deploying foo'))
-})
+tap.test("ok", (t) => {
+  t.plan(2);
+  const testRunner = new MockTestRunner(require.resolve("./success"));
+  testRunner.run();
+  t.equal(testRunner.succeeded, true);
+  t.ok(testRunner.stdout.includes("Deploying foo"));
+});
--- vercel-deployment-task/test/success.ts
+++ vercel-deployment-task/test/success.ts
@@ -1,6 +1,6 @@
-import { TaskMockRunner } from 'azure-pipelines-task-lib/mock-run'
+import { TaskMockRunner } from "azure-pipelines-task-lib/mock-run";
 
-const taskMockRunner = new TaskMockRunner(require.resolve('../src/index'));
-taskMockRunner.setInput('vercelProject', 'foo');
-taskMockRunner.setInput('vercelToken', 'bar');
+const taskMockRunner = new TaskMockRunner(require.resolve("../src/index"));
+taskMockRunner.setInput("vercelProject", "foo");
+taskMockRunner.setInput("vercelToken", "bar");
 taskMockRunner.run();
